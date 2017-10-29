c30f0578cf86437d93847c02ef9d523b8adeb931
Christian7007 <christig@ucm.es>
2017-10-29 17:29:24 +0100

    Añadidas las clases necesarias para la interaccion con BLE

---
+++ TFGParkinson/app/src/main/java/tfg/ucm/com/tfgparkinson/Clases/BLE/MultiBLECallback.java
@@ -0,0 +1,233 @@
+package tfg.ucm.com.tfgparkinson.Clases.BLE;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothProfile;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.util.SparseArray;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Custom implementation of the Bluetooth GATT Callbacks for connection with multiple devices.
+ * Created by silvia.valdez@hunabsys.com on 16/07/16.
+ */
+public class MultiBLECallback extends BluetoothGattCallback {
+
+    private static final String TAG = MultiBLECallback.class.getSimpleName();
+
+    private final byte[] ENCENDER_ACC = new byte[]{0x38, 0x02};
+    private final byte[] ENCENDER_ACC_GYRO = new byte[]{0x3f, 0x02};
+
+    // State Machine Tracking
+    private Handler mHandler;
+    private int mBleServiceId;
+    private List<Integer> mBleSensors;
+    private SparseArray<Integer> mCurrentSensors;
+
+
+    // Multi BLE Callback public constructor.
+    public MultiBLECallback(Handler handler) {
+        mHandler = handler;
+        mCurrentSensors = new SparseArray<>();
+
+        // Array containing the device's available sensor(s) to read. In our case, only accelerometer.
+        mBleSensors = new ArrayList<>();
+        mBleSensors.add(IMultiBLEMessageType.ACCELEROMETER_SERVICE);
+        // The ID of the first sensor.
+        mBleServiceId = mBleSensors.get(0);
+    }
+
+    @Override
+    public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+        Log.e(TAG, String.format("Connection State Change: %d -> %s",
+                status, connectionState(newState)));
+        if (status == BluetoothGatt.GATT_SUCCESS && newState == BluetoothProfile.STATE_CONNECTED) {
+                /*
+                 * Once successfully connected, we must next discover all the services on the
+                 * device before we can read and write their characteristics.
+                 */
+            gatt.discoverServices();
+            mHandler.sendMessage(Message.obtain(null,
+                    IMultiBLEMessageType.PROGRESS,
+                    "Discovering Services..."));
+        } else if (status == BluetoothGatt.GATT_SUCCESS
+                && newState == BluetoothProfile.STATE_DISCONNECTED) {
+                /*
+                 * If at any point we disconnect, send a message to clear the weather values
+                 * out of the UI
+                 */
+            mHandler.sendEmptyMessage(IMultiBLEMessageType.CLEAR);
+        } else if (status != BluetoothGatt.GATT_SUCCESS) {
+                /*
+                 * If there is a failure at any stage, simply disconnect
+                 */
+            gatt.disconnect();
+        }
+    }
+
+    @Override
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+        Log.d(TAG, "Services Discovered: " + status);
+        mHandler.sendMessage(Message.obtain(null, IMultiBLEMessageType.PROGRESS, "Enabling Sensors..."));
+            /*
+             * With services discovered, we are going to bleServiceReset our state machine
+             * and start working through the sensors we need to enable
+             */
+        bleServiceReset();
+        enableNextSensor(gatt);
+    }
+
+    @Override
+    public void onCharacteristicRead(BluetoothGatt gatt,
+                                     BluetoothGattCharacteristic characteristic,
+                                     int status) {
+        // Nothing to do here
+    }
+
+    @Override
+    public void onCharacteristicWrite(BluetoothGatt gatt,
+                                      BluetoothGattCharacteristic characteristic,
+                                      int status) {
+        // After writing the enable flag, next we read the initial value
+        setNotifyNextSensor(gatt);
+    }
+
+    @Override
+    public void onCharacteristicChanged(BluetoothGatt gatt,
+                                        BluetoothGattCharacteristic characteristic) {
+        /*
+         * After notifications are enabled, all updates from the device on characteristic value
+         * changes will be posted here. We hand these up to the UI thread to update the display.
+         */
+        BluetoothGattDto bluetoothGattDto = new BluetoothGattDto(gatt, characteristic);
+
+        if (isAccelerometerChar(characteristic.getUuid())) {
+            mHandler.sendMessage(Message.obtain(null,
+                    IMultiBLEMessageType.ACCELEROMETER_MESSAGE, bluetoothGattDto));
+        }
+    }
+
+    @Override
+    public void onDescriptorWrite(BluetoothGatt gatt,
+                                  BluetoothGattDescriptor descriptor,
+                                  int status) {
+        // Once notifications are enabled, we move to the next sensor and start over with enable
+        bleNextService(gatt);
+        enableNextSensor(gatt);
+    }
+
+    @Override
+    public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
+        Log.d(TAG, "Remote RSSI: " + rssi);
+    }
+
+    /*
+     * Send an enable command to each sensor by writing a configuration characteristic.
+     * This is specific to the SensorTag to keep power low by disabling sensors you aren't using.
+     */
+    private void enableNextSensor(BluetoothGatt gatt) {
+        BluetoothGattCharacteristic characteristic;
+        switch (mBleServiceId) {
+            case IMultiBLEMessageType.ACCELEROMETER_SERVICE:
+                Log.e(TAG, "Enabling accelerometer service...");
+                characteristic = gatt.getService(IMultiBLEMessageType.ACCEL_SERVICE)
+                        .getCharacteristic(IMultiBLEMessageType.ACCEL_CONFIG_CHAR);
+                characteristic.setValue(ENCENDER_ACC);
+                break;
+
+            default:
+                mHandler.sendEmptyMessage(IMultiBLEMessageType.DISMISS);
+                Log.e(TAG, String.format("All Sensors Enabled for %s!", gatt.getDevice()));
+                return;
+        }
+        gatt.writeCharacteristic(characteristic);
+    }
+
+    /*
+     * Enable notification of changes on the data characteristic for each sensor by writing
+     * the ENABLE_NOTIFICATION_VALUE flag to that characteristic's configuration descriptor.
+     */
+    private void setNotifyNextSensor(BluetoothGatt gatt) {
+        BluetoothGattCharacteristic characteristic;
+        switch (mBleServiceId) {
+            case IMultiBLEMessageType.ACCELEROMETER_SERVICE:
+                Log.e(TAG, "Set notify accelerometer sensor.");
+                characteristic = gatt.getService(IMultiBLEMessageType.ACCEL_SERVICE)
+                        .getCharacteristic(IMultiBLEMessageType.ACCEL_DATA_CHAR);
+                break;
+
+            default:
+                mHandler.sendEmptyMessage(IMultiBLEMessageType.DISMISS);
+                Log.e(TAG, String.format("All Sensors Notified for %s!", gatt.getDevice()));
+                return;
+        }
+
+        // Enable local notifications
+        gatt.setCharacteristicNotification(characteristic, true);
+
+        // Enabled remote notifications
+        BluetoothGattDescriptor descriptor =
+                characteristic.getDescriptor(IMultiBLEMessageType.CONFIG_DESCRIPTOR);
+        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+        gatt.writeDescriptor(descriptor);
+    }
+
+    private String connectionState(int status) {
+        switch (status) {
+            case BluetoothProfile.STATE_CONNECTED:
+                return "Connected";
+            case BluetoothProfile.STATE_DISCONNECTED:
+                return "Disconnected";
+            case BluetoothProfile.STATE_CONNECTING:
+                return "Connecting...";
+            case BluetoothProfile.STATE_DISCONNECTING:
+                return "Disconnecting...";
+            default:
+                return String.valueOf(status);
+        }
+    }
+
+    private void bleServiceReset() {
+        // Clear the list of read sensors and start again at the first one
+        mCurrentSensors.clear();
+        mBleServiceId = mBleSensors.get(0);
+    }
+
+    /*
+     * For each connected device, save a list of all the sensors activated
+     */
+    private void bleNextService(BluetoothGatt gatt) {
+        // Counter that indicates the sensor's position in our list of to-read sensors
+        Integer currentSensor;
+        // The hash code of the current device
+        int gattCode = gatt.hashCode();
+
+        if ((currentSensor = mCurrentSensors.get(gattCode)) != null) {
+            // If the device is already in the list, increases the counter of it's read sensors
+            mCurrentSensors.put(gattCode, ++currentSensor);
+        } else {
+            // If the device isn't in the list, initializes it's values
+            currentSensor = 0;
+            mCurrentSensors.put(gattCode, currentSensor);
+        }
+
+        // When all the sensors in a device were read, sets an unreachable value to mBleServiceId
+        if (currentSensor < mBleSensors.size()) {
+            mBleServiceId = mBleSensors.get(mCurrentSensors.get(gattCode));
+        } else {
+            mBleServiceId = 100;
+        }
+    }
+
+    private boolean isAccelerometerChar(UUID UuidChar) {
+        return UuidChar.toString().equals(IMultiBLEMessageType.ACCEL_DATA_CHAR.toString());
+    }
+
+}
---
+++ TFGParkinson/app/src/main/java/tfg/ucm/com/tfgparkinson/Clases/BLE/MultiBLEHandler.java
@@ -0,0 +1,129 @@
+package tfg.ucm.com.tfgparkinson.Clases.BLE;
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * Custom Handler implementation to process Message and Runnable
+ * of the BLE Callbacks on multiple devices.
+ * Created by silvia.valdez@hunabsys.com on 7/20/16.
+ */
+public class MultiBLEHandler extends Handler {
+
+    private static final String TAG = MultiBLEHandler.class.getSimpleName();
+
+    private IMultiBLEAccelDataReceiverDelegate mDelegate;
+    private ProgressDialog mProgressDialog;
+    private boolean mStopCapture;
+
+    // Public constructor of Multi BLE Handler.
+    public MultiBLEHandler(ProgressDialog dialog, IMultiBLEAccelDataReceiverDelegate delegate) {
+        // Delegate of the IMultiBLEAccelDataReceiverDelegate class to update the view.
+        this.mDelegate = delegate;
+
+        this.mProgressDialog = dialog;
+        this.mStopCapture = false;
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        BluetoothGattDto bluetoothGattDto;
+        BluetoothGatt gatt = null;
+        BluetoothGattCharacteristic characteristic = null;
+        int subject = msg.what;
+
+        if (subject == IMultiBLEMessageType.STOP_CAPTURE && !mStopCapture) {
+            mStopCapture = true;
+        } else if (subject == IMultiBLEMessageType.RESUME_CAPTURE) {
+            mStopCapture = false;
+        }
+
+        if (!mStopCapture) {
+            if (msg.obj instanceof BluetoothGattDto) {
+                // Custom DTO to save the data of the gatt and the characteristic read
+                bluetoothGattDto = (BluetoothGattDto) msg.obj;
+                gatt = bluetoothGattDto.getBluetoothGatt();
+                characteristic = bluetoothGattDto.getBluetoothCharacteristic();
+            }
+
+            switch (msg.what) {
+                case IMultiBLEMessageType.PROGRESS:
+                    mProgressDialog.setMessage((String) msg.obj);
+                    if (!mProgressDialog.isShowing()) {
+                        mProgressDialog.show();
+                    }
+                    break;
+
+                case IMultiBLEMessageType.ACCELEROMETER_MESSAGE:
+                    if (characteristic == null || characteristic.getValue() == null) {
+                        Log.w(TAG, "Error obtaining accelerometer value");
+                        return;
+                    }
+                    updateAccelerometerValue16(gatt, characteristic);
+                    break;
+
+                case IMultiBLEMessageType.DISMISS:
+                    mProgressDialog.hide();
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Update accelerometer value base 16, if implemented.
+     *
+     * @param characteristic the characteristic read.
+     */
+    private void updateAccelerometerValue16(BluetoothGatt gatt,
+                                            BluetoothGattCharacteristic characteristic) {
+        if (mDelegate != null) {
+            int[] accelerometer = getAccelData16(characteristic);
+            mDelegate.updateAccelerometer(gatt, accelerometer[0], accelerometer[1],
+                    accelerometer[2], accelerometer[3], accelerometer[4], accelerometer[5]);
+        }
+    }
+
+    public int[] getAccelData16(BluetoothGattCharacteristic characteristic) {
+        /*bytes   data
+        * 0,1   >> GyroX
+        * 2,3   >> GyroY
+        * 4,5   >> GyroZ
+        * 6,7   >> AcclX
+        * 8,9   >> AcclY
+        * 10,11 >> AcclZ
+        * 12,13 >> MagnX
+        * 14,15 >> MagnY
+        * 16,17 >> MagnZ
+        * */
+
+        int[] result = new int[6];
+        byte[] value = characteristic.getValue();
+
+        // Three first values for the accelerometer data
+        result[0] = getIntFromByteArray(value, 6);
+        result[1] = getIntFromByteArray(value, 8);
+        result[2] = getIntFromByteArray(value, 10);
+        // Three last values for the gyroscope data
+        result[3] = getIntFromByteArray(value, 0);
+        result[4] = getIntFromByteArray(value, 2);
+        result[5] = getIntFromByteArray(value, 4);
+
+        return result;
+    }
+
+    private int getIntFromByteArray(byte[] byteArray, int offset) {
+        int result, high, low;
+
+        high = byteArray[offset];
+        low = byteArray[offset + 1];
+        result = ((high & 0x000000FF) << 8) | (low & 0x000000FF);
+        result = (result > 32767) ? result - 65536 : result;
+
+        return result;
+    }
+
+}
